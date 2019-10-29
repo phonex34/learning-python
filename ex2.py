6bd3038b4257aad6a968f6deaafed61b5ed159d3
phonex <peon.phonex@gmail.com>
2019-10-30 01:37:08 +0700

    ex2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# Define the release and runtime integer variables below:
+
+release_year = 1
+runtime = 2.0
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = release_year + runtime
+a_float = 2.1
+
+print(rating_out_of_10)
